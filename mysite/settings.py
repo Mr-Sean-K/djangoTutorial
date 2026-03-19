28a675bc91a46d1ab94503e7a4e34183b28de5e4
Sean Kelly <b00147339@mytudublin.ie>
2026-03-19 21:58:43 +0000

    changed settings.py for server hosting

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7i9$yu4f9$h0m$__-)-!ty$w^2m@1xe@bztedl7q(h-8$5a4p*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["sean17111.pythonanywhere.com"]
 
 
 # Application definition
@@ -117,3 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
